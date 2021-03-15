cc42dee545548370391bb72db45eed0a8d39d854
Kara Kelly <ksquared9595@gmail.com>
2021-03-14 19:42:43 -0500

    workshop 2

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,23 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    const currentDate = new Date();
+    newComment.date = currentDate.toISOString();
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+}
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
